189fa8d4ce0d51a5afe47d557dc839070e37ed90
jmsparrowk <jmsparrowk@naver.com>
2024-07-22 21:43:39 +0900

    7/22

---
+++ src/main/java/Login/test/entity/UserEntity.java
@@ -0,0 +1,24 @@
+package Login.test.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@NoArgsConstructor //모든 필드를 받아야 함으로 기본 생성자는 필요하지 않다고 판단
+@AllArgsConstructor //모든 필드를 매개변수로 하는 생성자
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String facilityCode;
+    private String name;
+    private String password;
+
+}
