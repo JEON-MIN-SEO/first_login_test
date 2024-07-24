b458922ac40fe9059f7918db91f1c4c621a53193
jmsparrowk <jmsparrowk@naver.com>
2024-07-24 22:45:47 +0900

    7/24

---
+++ src/main/java/Login/test/SecurityConfig.java
@@ -0,0 +1,52 @@
+package Login.test;
+
+import Login.test.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/register", "/login", "/css/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userService;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+}
--- src/main/java/Login/test/service/UserService.java
+++ src/main/java/Login/test/service/UserService.java
@@ -1,4 +1,26 @@
 package Login.test.service;
 
+import Login.test.dto.UserDTO;
+import Login.test.entity.UserEntity;
+import Login.test.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void save(UserDTO userDTO){
+        UserEntity userEntity = new UserEntity();
+        userEntity.setName(userDTO.getName());
+        userEntity.setFacilityCode(userDTO.getFacilityCode());
+        userEntity.setPassword(userDTO.getPassword());
+        userRepository.save(userEntity);
+    }
+
+    private UserEntity findByName(String name){
+        return userRepository.findByName(name);
+    }
 }
